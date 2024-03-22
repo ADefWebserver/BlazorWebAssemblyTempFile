39c2d1973f46018ffd7748d1c7270efd3d3b3bcd
Michael Washington <webmaster@adefwebserver.com>
2024-03-22 06:17:38 -0700

    fixing

---
+++ Program.cs
@@ -0,0 +1,22 @@
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace BlazorWebAssemblyTempFile
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddBlazoredLocalStorage();
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# BlazorWebAssemblyTempFile
+### Demonstrates using WebAssembly session file storage
+### Demonstrates using LocalStorage to persist files
+![image](https://github.com/ADefWebserver/BlazorWebAssemblyTempFile/assets/1857799/c15b395d-bee2-4093-8390-985a1ef20baf)
